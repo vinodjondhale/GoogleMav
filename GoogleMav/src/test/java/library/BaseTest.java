795ecdc76753bba127f88a5e8f701ba334f06e55
Vinod J <Vinod J@Vinod>
2018-09-02 17:06:17 +0530

    Commit with Multiple browsers

--- GoogleMav/src/test/java/library/BaseTest.java
+++ GoogleMav/src/test/java/library/BaseTest.java
@@ -4,19 +4,29 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class BaseTest implements Constants {
 public WebDriver driver;
 @BeforeMethod
-public void before() 
+@Parameters("myBrowser")
+public void before(String myBrowser) 
 {
-System.setProperty(CHROME_DRIVER, CHROME_PATH);
-driver= new ChromeDriver();
-driver.get(URL);
-driver.manage().window().maximize();
-driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	if(myBrowser.equalsIgnoreCase("firefox"))
+	{
+		System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+	else if (myBrowser.equalsIgnoreCase("chrome")) {
+		System.setProperty(CHROME_DRIVER, CHROME_PATH);
+		driver= new ChromeDriver();	
+	}
+	
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.get(URL);
 }
 @AfterMethod
 public void close()
